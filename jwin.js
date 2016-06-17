6ef74254350ec9a27de07a916e5c3c4886633aef
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-06-17 20:58:54 +0200

    Basic functions

---
+++ jwin.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Stefan Wimmer <stefanwimmer128@gmail.com> on 17.06.16.
+ */
+
+let $ = {};
+
+$ = require("./lib/exception")($);
+$ = require("./lib/group")($);
+
+module.exports = $;
